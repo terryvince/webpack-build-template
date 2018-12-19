425e61e39164190c16621e715ad45f914d382d50
terry <2931361912@qq.com>
2018-12-19 10:10:19 +0800

    update

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-// parser: 'sugarss',   // css语法风格检测
-    plugins: {
-        // 'postcss-import': {},
-        // 'postcss-preset-env': {}, // 使用现代css语法
-        'autoprefixer': {}, // 添加厂商前缀
-        'cssnano': {} // 代码压缩，优化
-    }
-}
