35f7e4af108f773fdec14005ea57ebf991d3d6d1
terry <2931361912@qq.com>
2018-12-20 15:35:34 +0800

    add utils/brower.js

---
+++ src/utils/brower.js
@@ -0,0 +1,24 @@
+export default {
+    versions:function(){
+        let u = navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') === -1,//火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+            iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') === -1, //是否web应该程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') > -1, //是否微信
+            qq: u.match(/\sQQ/i) === ' qq' //是否QQ
+        };
+    }(),
+    appVersion: navigator.appVersion,
+    // 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36
+    language:(navigator.browserLanguage || navigator.language).toLowerCase()
+};
+
+// browers.versions.weixin == true 则使用的是微信
