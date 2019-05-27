6ca308952020e93ca096df14e2ca8e04aa89b95e
terry <2931361912@qq.com>
2019-05-27 14:51:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -21,6 +21,11 @@ $(function () {
 
 
 //if you need vue ，use it
+const isProd = false;
+// cancel all warning
+vue.config.silent=isProd;
+// stop all of the vue production tip when start
+vue.config.productionTip=!isProd;
 const vue = new Vue({
     el: '#app',
     components: {App},
